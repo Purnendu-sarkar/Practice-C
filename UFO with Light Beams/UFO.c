02923c04dc50ce614d53111081fae4aa18ccf039
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-05-30 17:35:25 +0600

    UFO with Light Beams

---
+++ UFO with Light Beams/UFO.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    int width = 21;
+
+    // Top Dome
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j >= width / 2 - i && j <= width / 2 + i)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    // UFO Disc Body
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0 || j == width - 1)
+                printf("*");
+            else
+                printf("=");
+        }
+        printf("\n");
+    }
+
+    // Beam lights
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == width / 2 - i || j == width / 2 + i)
+                printf("|");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
