177088c026963ff64ade75dcede7df2c945b9a94
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-06-06 19:43:16 +0600

    Create DNAHelix.c

---
+++ DNAHelix.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 20;
+
+    for (int i = 0; i < rows; i++) {
+        int mod = i % 6;
+        switch (mod) {
+            case 0:
+                printf("A---T\n");
+                break;
+            case 1:
+                printf(" A---T\n");
+                break;
+            case 2:
+                printf("  A---T\n");
+                break;
+            case 3:
+                printf("   T---A\n");
+                break;
+            case 4:
+                printf("  T---A\n");
+                break;
+            case 5:
+                printf(" T---A\n");
+                break;
+        }
+    }
+
+    return 0;
+}
