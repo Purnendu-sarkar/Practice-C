6a0c56324e22bd946e3db425dd09623479d1ae36
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-11 16:36:28 +0600

    Compass_Rose_C

---
+++ Compass Rose/Compass_Rose.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("        N        \n");
+    printf("        |        \n");
+    printf("   NW  \\|/  NE   \n");
+    printf("W------*------E  \n");
+    printf("   SW  /|\\  SE   \n");
+    printf("        |        \n");
+    printf("        S        \n");
+
+    return 0;
+}
