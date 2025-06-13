c6f541ce8605384083e38913a9095e8931600298
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-13 17:08:24 +0600

    Torii Gate Pattern

---
+++ Torii Gate/ToriiGate.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("###################\n");
+    printf("#                 #\n");
+    printf("#  |         |    #\n");
+    printf("   |         |     \n");
+    printf("   |         |     \n");
+    printf("   |         |     \n");
+    printf("   |         |     \n");
+    printf("   |         |     \n");
+    printf("  /|\\       /|\\   \n");
+    printf(" /_|_\\     /_|_\\  \n");
+
+    return 0;
+}
