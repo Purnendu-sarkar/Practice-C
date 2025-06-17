07b8de79e2955c8ce9d0afd3bdfc7a81e570c228
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-17 15:31:42 +0600

    Evil Eye

---
+++ Evil Eye/EvilEye.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("     *****     \n");
+    printf("   *********   \n");
+    printf("  ***     ***  \n");
+    printf(" **   O O   ** \n");
+    printf("***    \\_/    ***\n");
+    printf(" **         ** \n");
+    printf("  ***     ***  \n");
+    printf("   *********   \n");
+    printf("     *****     \n");
+
+    return 0;
+}
