e07ebfa967a6ddcb62832257866f9bb8005ceab1
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-19 16:48:38 +0600

    Brain Network

---
+++ Brain Network/BrainNetwork.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("      #####      #####      \n");
+    printf("   ###     ### ###    ###   \n");
+    printf("  ##   0     ###     0  ##  \n");
+    printf(" ##    ###       ###    ## \n");
+    printf("##      ###     ###      ##\n");
+    printf("##        ##   ##        ##\n");
+    printf("##         #####         ##\n");
+    printf(" ##                   ###  \n");
+    printf("  ##     === ===     ##    \n");
+    printf("   ###           #####     \n");
+    printf("     #############         \n");
+
+    return 0;
+}
