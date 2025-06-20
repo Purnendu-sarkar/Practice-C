dfd3d1938d56d5a1267761ccc57ab4da414344bc
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-20 17:51:58 +0600

    Dragon Head

---
+++ Dragon Head/DragonHead.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("           / \\  //\\\n");
+    printf("     |\\___/|      \\\n");
+    printf("     /O  O \\       \\\n");
+    printf("    /     /|\\_     \\\n");
+    printf("   /  \\_/  || \\____|\n");
+    printf("  /        || |    |\n");
+    printf(" |   ______||_|___/\n");
+    printf("  \\_/      ||\n");
+    printf("     |_____/|\n");
+    printf("     |  ||  |\n");
+    printf("    (__||__)\n");
+    printf("     ^^  ^^ \n");
+
+    return 0;
+}
