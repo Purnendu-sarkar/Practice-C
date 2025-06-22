1964ed403bd514b776683e32f5284c4b3e54a8c4
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-22 17:22:36 +0600

    Rocket Ship

---
+++ Rocket Ship/RocketShip.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("     /\\     \n");
+    printf("    /  \\    \n");
+    printf("   / || \\   \n");
+    printf("  /  ||  \\  \n");
+    printf(" |   ||   | \n");
+    printf(" |   ||   | \n");
+    printf(" |   ||   | \n");
+    printf(" |   ||   | \n");
+    printf(" |   ||   | \n");
+    printf(" |   ||   | \n");
+    printf(" |   ||   | \n");
+    printf(" |___||___| \n");
+    printf("  |__||__|  \n");
+    printf("   /_/\\_\\   \n");
+    printf("   \\_||_/   \n");
+    printf("    ||||    \n");
+    printf("   ~~~~~~   \n");
+
+    return 0;
+}
