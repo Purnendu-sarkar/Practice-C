4300f1565d4757460330844ac3b8a6499c961db7
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-24 20:20:07 +0600

    Magic Hat

---
+++ Magic Hat/MagicHat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("      _____      \n");
+    printf("     /     \\     \n");
+    printf("    /_______\\    \n");
+    printf("    |       |    \n");
+    printf("    |       |    \n");
+    printf("    |_______|    \n");
+    printf("  /___________\\  \n");
+    printf(" /             \\ \n");
+    printf("~~~~~~~~~~~~~~~~~\n");
+
+    return 0;
+}
