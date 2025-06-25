8389f21aabe67026f2ab471d72fb2b98ef8261a5
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-25 15:22:19 +0600

    Gear Mechanism

---
+++ Gear Mechanism/GearMechanism.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("      _---_      \n");
+    printf("   /         \\   \n");
+    printf("  |  ()   ()  |  \n");
+    printf(" |     |_|     | \n");
+    printf("  |   \\___/   |  \n");
+    printf("   \\_________/   \n");
+    printf("   /  /   \\  \\   \n");
+    printf("  |  |     |  |  \n");
+    printf("   \\_|     |_/   \n");
+    printf("     \\_____/     \n");
+
+    return 0;
+}
