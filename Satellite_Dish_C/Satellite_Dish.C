ffbf47b2e3b825c6b7ca016737d1ce77336228f0
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-27 12:06:17 +0600

    Satellite_Dish_C

---
+++ Satellite_Dish_C/Satellite_Dish.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("         /\\        \n");
+    printf("        /  \\       \n");
+    printf("       /----\\      \n");
+    printf("      /      \\     \n");
+    printf("     /        \\    \n");
+    printf("    /__________\\   \n");
+    printf("        |||        \n");
+    printf("       /|||\\       \n");
+    printf("      / ||| \\      \n");
+    printf("     /  |||  \\     \n");
+    printf("        |||        \n");
+    printf("       (___)       \n");
+
+    return 0;
+}
