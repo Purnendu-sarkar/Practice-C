c71776d126efe0bb5d88425efd257a8ba21729c6
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-29 11:40:53 +0600

    Carousel

---
+++ Carousel/Carousel.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("    _______\n");
+    printf("   /       \\\n");
+    printf("  /---------\\\n");
+    printf("     |||||\n");
+    printf("  __/|||||\\__\n");
+    printf(" /  o     o  \\\n");
+    printf("|    CAROUSEL |\n");
+    printf(" \\___________/");
+
+    return 0;
+}
