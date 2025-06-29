9d2f85dc87c3cd73a088f071fb5efea115d6b4d1
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-06-30 00:09:12 +0600

    Chemistry Flask

---
+++ Chemistry Flask/Chemistry Flask.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("     ____\n");
+    printf("    /    \\\n");
+    printf("   /      \\\n");
+    printf("   \\      /\n");
+    printf("    \\____/\n");
+    printf("     ||||\n");
+    printf("     ||||\n");
+    printf("    (====)\n");
+    printf("    (____)");
+
+    return 0;
+}
