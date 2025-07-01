107e6b8223bc54fceade01db98b57ffe669888b7
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-07-01 18:58:21 +0600

    Open Book

---
+++ Open Book/Open Book.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("     ______    ______\n");
+    printf("    /     /\\  /\\     \\\n");
+    printf("   /     /__\\/__\\     \\\n");
+    printf("  |     |          |     |\n");
+    printf("  |     |  LEARN   |     |\n");
+    printf("  |     |__________|     |\n");
+    printf("   \\____________________/\n");
+
+    return 0;
+}
