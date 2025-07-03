aaa96b742942390495b752c5019cc33423e00409
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-07-04 00:12:36 +0600

    Headphones

---
+++ Headphones/Headphones.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("    _________    \n");
+    printf("   /         \\  \n");
+    printf("  |  O     O  | \n");
+    printf("  |           | \n");
+    printf("   \\_________/  \n");
+    printf("     |     |    \n");
+    printf("     |     |    \n");
+
+    return 0;
+}
