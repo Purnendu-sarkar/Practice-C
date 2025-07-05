3636c1985e5b4bffe4f9507604673a90f38ad401
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-07-05 21:35:33 +0600

    Computer Monitor

---
+++ Computer Monitor/Computer Monitor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("  ____________________  \n");
+    printf(" |                    | \n");
+    printf(" |   Hello, World!    | \n");
+    printf(" |____________________| \n");
+    printf("     |            |     \n");
+    printf("     |   ______   |     \n");
+    printf("     |__|    |__|__|    \n");
+
+    return 0;
+}
