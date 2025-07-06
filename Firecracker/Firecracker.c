0fae2f1078e6c1608eeb15d29b7c8c4e866af442
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-07-06 21:54:33 +0600

    Firecracker

---
+++ Firecracker/Firecracker.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("     |\n");
+    printf("    / \\\n");
+    printf("   /___\\\n");
+    printf("    |||\n");
+    printf("    |||\n");
+    printf("   /|||\\\n");
+    printf("  * ||| *\n");
+    printf(" *  |||  *\n");
+    printf("*___|||___*\n");
+
+    return 0;
+}
